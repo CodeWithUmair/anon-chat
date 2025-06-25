8696f50bfa80f810d06500712587c1489ad7b314
CodeWithUmair <codewithumair867@gmail.com>
2025-06-26 03:41:04 +0500

    deployment fixes

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:8080/api/v1" // For Local
-// export const BACKEND_URL = "http://localhost:8080/api/v1" // For Live
+// export const BACKEND_URL = "http://localhost:8080/api/v1" // For Local
+export const BACKEND_URL = "https://be-anonchat.vercel.app/api/v1" // For Live
